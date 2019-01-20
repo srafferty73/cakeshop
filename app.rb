4005bdfaa674af8d31d86c711cbc12beee458670
Stephen Rafferty <srafferty73@gmail.com>
2019-01-20 22:16:53 +0000

    delete page added (incomplete)

--- app.rb
+++ app.rb
@@ -12,11 +12,18 @@ end
 
 # INDEX
 get '/cake-inventory' do
-  # Go to the database and retrieve all the orders & manufacturers
-    @bakeries = Manufacturer.all
+  # Go to the database and retrieve all the orders
     @orders = Cake.all
-  # Display a list of all the orders and manufacturers
+  # Display a list of all the orders
   erb( :index )
+end
+
+# MANUFACTURER INDEX
+get '/cake-orders/show-bakeries' do
+  # Go to the database and retrieve all the manufacturers
+    @bakeries = Manufacturer.all
+  # Display a list of all the manufacturers
+  erb( :manufacturer_index )
 end
 
 # NEW
@@ -38,3 +45,19 @@ end
 # EDIT
 # UPDATE
 # DELETE
+
+get '/cake-orders/delete-cake' do
+  erb( :delete)
+end
+
+post '/cake-orders/delete-cake' do
+  delete()
+  redirect to '/cake-inventory'
+end
+
+def delete()
+  sql = "DELETE FROM cakes
+  WHERE id = $1"
+  values = [@id]
+  SqlRunner.run( sql, values )
+end
